0d47ab5e234b2e0d1fc6bd5db32901575b269ffe
WIN-1V5CRJ22OC3\User <tomasz.banach89@gmail.com>
2021-02-26 10:55:15 +0100

    ConfigurationFile

---
+++ SpecFlowUBS/UBS.Commons/ConfigMasterObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UBS.Commons
+{
+    public class ConfigMasterObject
+    {
+        public string ApplicationURL { get; set; }
+        public bool ChromeHeadless { get; set; }
+    }
+}
